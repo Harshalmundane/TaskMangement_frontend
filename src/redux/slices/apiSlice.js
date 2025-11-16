5a0654f6b2b45ad1f2a29a23a1f09f8c73ec47ae
Harshalmundane <harshalmundane01@gmail.com>
2025-11-16 12:09:23 +0530

    fix the vite app base url

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -1,9 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
 
-const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: API_URL,
+  prepareHeaders: (headers, { getState }) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
